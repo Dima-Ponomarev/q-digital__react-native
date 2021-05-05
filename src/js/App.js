bf35d8a50a820fd8f1b75a4ec40c022001629167
Dima Ponomarev <kikkyow18@gmail.com>
2021-05-05 12:02:27 +0400

    fixes

--- src/js/App.js
+++ src/js/App.js
@@ -3,19 +3,31 @@ import Main from './screens/main'
 import Slider from './screens/slider'
 import Navigation from './components/navigation'
 import Player from './screens/player'
-import { View } from 'react-native'
+import { View, AppState } from 'react-native'
 import { NativeRouter as Router, Switch, Route } from 'react-router-native'
 import store from './redux/index'
-import { setPlayerStatus, setPlayingStatus } from './redux/actions'
+import { setCurrentIndex, setPlayerStatus, setPlayingStatus } from './redux/actions'
 import { Provider } from 'react-redux' 
 import TrackPlayer from 'react-native-track-player'
 
 
 export class App extends Component {
 
+  componentDidMount(){
+    AppState.addEventListener('change', this.handleAppStateChange)
+  }
+
   componentWillUnmount(){
-    store.dispatch(setPlayingStatus(false))
-    TrackPlayer.pause()
+    AppState.removeEventListener('change', this.handleAppStateChange)
+    store.dispatch(setPlayerStatus(false))
+    store.dispatch(setCurrentIndex(0))
+  }
+
+  handleAppStateChange = (nextAppState) => {
+    if (nextAppState.match(/inactive|background/)){
+      store.dispatch(setPlayingStatus(false))
+      TrackPlayer.pause()
+    }
   }
 
   render() {
--- src/js/services/trackPlayerService.js
+++ src/js/services/trackPlayerService.js
@@ -2,22 +2,26 @@ import TrackPlayer from 'react-native-track-player'
 import store from '../redux/index'
 import { setPlayingStatus, setPlayerStatus, setCurrentIndex } from '../redux/actions'
 
+let listenersAdded = false
 
 module.exports = async function () {
+    if (listenersAdded){
+        console.log('listeners already added')
+        return
+    }
+    listenersAdded = true
+
     TrackPlayer.addEventListener('remote-play', () => {
-        console.log('play')
         store.dispatch(setPlayingStatus(true))
         TrackPlayer.play()
     });
 
     TrackPlayer.addEventListener('remote-pause', () => {
-        console.log('pause')
         store.dispatch(setPlayingStatus(false))
         TrackPlayer.pause()
     });
 
     TrackPlayer.addEventListener('remote-next', async () => {
-        console.log('next')
         const trackId = await TrackPlayer.getCurrentTrack()
         const state = store.getState().playlist
         if (trackId === state.playlist[state.playlist.length - 1].id){
@@ -29,7 +33,6 @@ module.exports = async function () {
     })
 
     TrackPlayer.addEventListener('remote-previous', async () => {
-        console.log('previous')
         const trackId = await TrackPlayer.getCurrentTrack()
         const state = store.getState().playlist
         if (trackId === state.playlist[0].id){
